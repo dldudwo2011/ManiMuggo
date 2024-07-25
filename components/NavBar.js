5957a1b1bea7b362e9d57786379385d39bcdd377
Karthik Saride <32055885+karthikkraze@users.noreply.github.com>
2024-07-24 21:12:55 -0400

    24th July 2024 - Fixed the design issues in driver registration page

--- components/NavBar.js
+++ components/NavBar.js
@@ -107,15 +107,15 @@ export default function NavBar(params) {
           </button>
         </div>
         <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-language">
-          <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:bg-white md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 hover:underline">
+          <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:bg-white md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 ">
             <li>
-              <a href="/" class="block py-2 px-3 text-black bg-green-400 rounded md:bg-transparent md:text-green-400 md:p-0 md:dark:text-green-400 hover:underline" aria-current="page">Home</a>
+              <a href="/" class="block py-2 px-3 text-black bg-green-400 rounded md:bg-transparent md:text-green-400 md:p-0 md:dark:text-green-400" aria-current="page">Home</a>
             </li>
             <li>
-              <a href="/about" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-400 dark:text-white md:dark:hover:text-green-400 hover:underline dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ">About</a>
+              <a href="/about" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-400 dark:text-white md:dark:hover:text-green-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ">About</a>
             </li>
             <li>
-              <a href="/joinDriver" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-400 dark:text-white md:dark:hover:text-green-400 hover:underline dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ">Careers</a>
+              <a href="/joinDriver" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-400 dark:text-white md:dark:hover:text-green-400  dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ">Careers</a>
             </li>
           </ul>
         </div>
--- pages/driver.js
+++ pages/driver.js
@@ -209,7 +209,7 @@ const DriverRegistration = () => {
 
   return   (
     <div className="max-w-3xl mx-auto p-6 bg-white shadow-xl rounded-xl mt-8 mb-6 grid grid-cols-1 md:grid-cols-2 gap-4">
-      <div className="hidden md:flex items-center justify-center col-span-1">
+      <div className="hidden h-screen md:flex items-center justify-center col-span-1">
         <img
          src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-content-2.png" alt="office image 2"
           className="w-lg h-auto object-cover rounded-xl"
@@ -221,7 +221,7 @@ const DriverRegistration = () => {
           <form onSubmit={handleSubmit} className="space-y-6">
             {step === 0 && (
               
-              <div className="form-step">
+              <div className="form-step h-screen">
                 <h2 className="text-2xl font-bold mb-6">Step 1: Personal Information</h2>
                 <div className="space-y-4">
                   <label className="block">
@@ -317,7 +317,7 @@ const DriverRegistration = () => {
               </div>
             )}
             {step === 1 && (
-              <div className="form-step">
+              <div className="form-step h-screen">
                 <h2 className="text-2xl font-bold mb-6">Step 2: Vehicle Information</h2>
                 <div className="space-y-4">
                   <label className="block">
@@ -407,12 +407,53 @@ const DriverRegistration = () => {
               </div>
             )}
             {step === 2 && (
-              <div className="form-step">
+              <div className="form-step h-screen">
                 <h2 className="text-2xl font-bold mb-6">Step 3: ID Verification</h2>
-                <input type="text" placeholder="First Name" value={firstName} onChange={(e) => setFirstName(e.target.value)} required />
-                <input type="text" placeholder="Last Name" value={lastName} onChange={(e) => setLastName(e.target.value)} required />
-                <input type="file" onChange={(e) => setFile(e.target.files[0])} required />
-                <button type="submit">Verify</button>
+
+                <div className="space-y-4">
+                  <label className="block">
+                    First Name:
+                    <input
+                      type="text"
+                      name="firstName"
+                      value={formData.firstName}
+                      onChange={handleChange}
+                      required
+                      className="mt-1 block w-full border border-gray-300 rounded-md p-2 shadow-sm"
+                    />
+                  </label>
+                  <label className="block">
+                    Last Name:
+                    <input
+                       type="text"
+                       name="lastName"
+                       value={formData.lastName}
+                       onChange={handleChange}
+                       required
+                      className="mt-1 block w-full border border-gray-300 rounded-md p-2 shadow-sm"
+                    />
+                  </label>
+                  <label className="block">
+                    Upload Driving License:
+                  <input type="file" onChange={(e) => setFile(e.target.files[0])} required />
+                  </label>
+                </div>
+                <div className="flex justify-between mt-6">
+                  <button
+                    type="button"
+                    onClick={prevStep}
+                    className="bg-gray-500 text-white py-2 px-4 rounded-md shadow-sm hover:bg-gray-600"
+                  >
+                    Previous
+                  </button>
+                  <button
+                    id="veriffButton"
+                    type="submit"
+                    className="bg-blue-500 text-white px-4 py-2 rounded-md shadow-sm hover:bg-blue-600"
+                  >
+                    Verify with Veriff
+                  </button>
+                </div>
               {verificationUrl && (
                 <div>
                   <h2>Verification URL</h2>
